5306b02415eb99a0207686302b1f0278acec9162
youngminz <youngminz.kr@gmail.com>
2014-10-25 19:32:37 +0000

    something many

--- board/exchange.php
+++ board/exchange.php
@@ -1,49 +1,36 @@
 <?php
 session_start();
 require_once('../config.php');
-if (!isset($_SESSION['ID'])) {
+require_once('../function.php');
+if (!isset($_SESSION['ID']) || empty($_SESSION['ID'])) {
     echo "<meta http-equiv='refresh' content='0; url=/login.php'>";
     exit;
 }
-
-function bind_array($stmt, &$row) {
-    $md = $stmt->result_metadata();
-    $params = array();
-    while ($field = $md->fetch_field()) {
-        $params[] = &$row[$field->name];
-    }
-    call_user_func_array(array($stmt, 'bind_result'), $params);
-}
-
-function get_user_name($id) {
-    global $db_host, $db_user, $db_pass, $db_name;
-    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-    if ($conn->connect_errno) {
-        echo "<h1>데이터베이스에 연결하던 도중 오류가 발생했습니다.</h1>";
-        exit;
-    }
-    $conn->set_charset('utf8');
-    $stmt = $conn->prepare("SELECT user_nickname FROM users WHERE ID=?");
-    $stmt->bind_param('i', $id);
-    $stmt->execute();
-    $stmt->store_result();
-
-    if ($stmt->num_rows > 0) {
-        bind_array($stmt, $row);
-        $stmt->fetch();
-        return $row['user_nickname'];
-    }
-    else {
-        return "Error";
-    }
-}
-
 $page = 1;
 if (isset($_GET['page'])) {
     $page = (int) $_GET['page'];
     if (!is_int($page)) {
         $page = 1;
     }
+}
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_errno) {
+    echo "<h1>데이터베이스에 연결하던 도중 오류가 발생했습니다.</h1>";
+    exit;
+}
+
+$first_row = fetch_first_row("SELECT COUNT(*) FROM exchange_article WHERE parent_id IS NULL");
+$number_of_row = $first_row['COUNT(*)'];
+if ($number_of_row % 10 != 0) {
+    $total_page_count = $number_of_row / 10 + 1;
+}
+else {
+    $total_page_count = $number_of_row / 10;
+}
+
+if ($page > $total_page_count || $page < 1) {
+    $page = 1;
 }
 ?>
 <!DOCTYPE html>
@@ -57,35 +44,12 @@ if (isset($_GET['page'])) {
 </head>
 <body>
 <?php
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-if ($conn->connect_errno) {
-    echo "<h1>데이터베이스에 연결하던 도중 오류가 발생했습니다.</h1>";
-    exit;
-}
-$conn->set_charset('utf8');
-
-$stmt = $conn->prepare("SELECT COUNT(*) FROM exchange_article WHERE parent_id IS NULL");
-$stmt->execute();
-$stmt->store_result();
-bind_array($stmt, $row);
-$stmt->fetch();
-$num_row = $row['COUNT(*)'];
-if ($page > ($num_row / 10) + 1 || $page < 1) {
-    $page = 1;
-}
-
-$stmt->close();
-
-$stmt = $conn->prepare("SELECT * FROM exchange_article " .
-                       "WHERE parent_id IS NULL " .
-                       "ORDER BY id DESC " .
-                       "LIMIT ?, 10");
+$stmt = $conn->prepare("SELECT * FROM exchange_article WHERE parent_id IS NULL ORDER BY id DESC LIMIT ?, 10");
 $start_num = ($page - 1) * 10;
 $stmt->bind_param('i', $start_num);
 $stmt->execute();
 $stmt->store_result();
 ?>
-
 <table>
     <thead>
     <tr>
@@ -101,11 +65,10 @@ $stmt->store_result();
     </tr>
     </thead>
     <tbody>
-    <?php
+    <?php 
     if ($stmt->num_rows > 0) {
         bind_array($stmt, $row);
-
-        while ($stmt->fetch()) {?>
+        while ($stmt->fetch()) { ?>
             <tr>
                 <td><?= $row['ID'] ?></td>
                 <td><?= $row['board_title'] ?></td>
@@ -115,12 +78,10 @@ $stmt->store_result();
                 <td><?= $row['board_hit'] ?></td>
                 <td><?= $row['vote_up'] ?></td>
                 <td><?= $row['vote_down'] ?></td>
-                <td><?= get_user_name($row['author']) ?></td>
+                <td><?= fetch_first_row('SELECT * FROM users WHERE ID=?', 'i', $row['author'])['user_nickname'] ?></td>
             </tr>
-        <?php
-        }
-    }
-    ?>
+  <?php }
+    } ?>
     </tbody>
 </table>
 <?php
@@ -128,7 +89,7 @@ $stmt->close();
 $conn->close();
 ?>
 <?php
-for ($i = 1; $i <= ($num_row / 10) + 1; $i++) {
+for ($i = 1; $i <= $total_page_count; $i++) {
     echo "<a href='/board/exchange.php?page=$i'>$i</a> ";
 }
 ?>
