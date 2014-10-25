32e12a6c12f25cb768ade20b43bd8ebeee26eb27
youngminz <youngminz.kr@gmail.com>
2014-10-25 19:31:02 +0000

    isset&&!empty

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['ID'])) {
+if (!isset($_SESSION['ID']) || empty($_SESSION['ID'])) {
     echo "<meta http-equiv='refresh' content='0; url=/login.php'>";
     exit;
 }
