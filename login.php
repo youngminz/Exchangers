a553591cc756be5557f58eac8c3f1c16a515f98e
Kuriyama hibiya <kuriyama@hibiya.moe>
2014-10-26 01:33:33 +0000

    typo; html rewrite

--- login.php
+++ login.php
@@ -20,12 +20,11 @@ $reason_error = '';
 if (isset($_SERVER['HTTP_REFERER'])) {
     if (strpos($_SERVER['HTTP_REFERER'], "weirdorithm.youngminz.kr/join.php") !== false) {
         $info = true;
-        $reason_info = "회원가입이 정상적으로 처리되었습니다. <br />" .
-            "회원가입 하신 아이디로 로그인 해 주세요! <br />";
+        $reason_info = "회원가입이 정상적으로 처리되었습니다. 가입하신 아이디로 로그인해주세요.";
     }
     if (strpos($_SERVER['HTTP_REFERER'], 'weirdorithm.youngminz.kr/logout.php') !== false) {
         $info = true;
-        $reason_info = "성공적으로 로그아웃되었습니다! <br />";
+        $reason_info = "성공적으로 로그아웃되었습니다.";
     }
 }
 
@@ -65,36 +64,37 @@ if ($_POST) {
 }
 
 if (isset($_SESSION['user_id']) && isset($_SESSION['user_nickname']) && isset($_SESSION['user_email'])) {
-    echo "<meta http-equiv='refresh' content='0; url=/'>";
+    echo "<meta http-equiv='refresh' content='0; url=/' />";
     exit;
 }
 
 require_once('header.php');
-
-if ($info === true) {
-    echo $reason_info;
-}
-if ($error === true) {
-    echo $reason_error;
-}
 ?>
-<form action="login.php" method="post">
-    <table cellspacing="0">
-        <tr>
-            <th>ID:</th>
-            <td><label><input type="text" name="user_id"/></label></td>
-        </tr>
-        <tr>
-            <th>Password:</th>
-            <td><label><input type="password" name="user_pass"/></label></td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <input type="submit" value="전송">
-                <a href="join.php">회원가입</a>
-            </td>
-        </tr>
-    </table>
-</form>
+<div class="container">
+  <h1>로그인</h1>
+<?php if ($info === true) { ?>
+  <p class="message-success">
+    <?= $reason_info ?>
+  </p>
+<?php } if ($error === true) { ?>
+  <p class="message-error">
+    <?= $reason_error ?>
+  </p>
+<?php } ?>
+  <form id="form-login" action="login.php" method="post">
+    <p class="form-line">
+      <label for="user_id">ID</label>
+      <input type="text" name="user_id" />
+    </p>
+    <p class="form-line">
+      <label for="user_pass">Password</label>
+      <input type="password" name="user_pass" />
+    </p>
+    <p class="form-line">
+      <input type="submit" value="로그인">
+      <a href="join.php">회원가입</a>
+    </p>
+  </form>
+</div>
 </body>
 </html>
