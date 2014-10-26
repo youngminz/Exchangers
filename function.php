92ac337d598672edc17735f127285003d85a1697
youngminz <youngminz.kr@gmail.com>
2014-10-26 05:47:33 +0000

    clobe_array / fetch_all_row / execute_query

--- function.php
+++ function.php
@@ -42,7 +42,16 @@ function bind_array($stmt, &$row) {
     call_user_func_array(array($stmt, 'bind_result'), $params);
 }
 
+function clone_array($arr) {
+    $cloned_array = [];
+    foreach ($arr as $k => $v) {
+        $cloned_array[$k] = $v;
+    }
+    return $cloned_array;
+}
+
 // First argument is sql query, second is bind_param mode, after third is parameters.
+// Return: success -> array, error -> false
 function fetch_first_row() {
     $args = func_get_args();
     $args_count = func_num_args();
@@ -91,6 +100,107 @@ function fetch_first_row() {
     else {
         $stmt->close();
         $conn->close();
-        return "Error";
+        return false;
     }
 }
+
+function fetch_all_row() {
+    $args = func_get_args();
+    $args_count = func_num_args();
+    if ($args_count == 1) {
+        $sql = $args[0];
+    }
+    else {
+        $sql = $args[0];
+        $mode = $args[1];
+    }
+    
+    global $db_host, $db_user, $db_pass, $db_name;
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    if ($conn->connect_errno) {
+        echo "<h1>데이터베이스에 연결하던 도중 오류가 발생했습니다.</h1>";
+        exit;
+    }
+    $stmt = $conn->prepare($sql);
+    if ($args_count != 1) {
+        if ($args_count == 3) {
+            $stmt->bind_param($mode, $args[2]);
+        }
+        else if ($args_count == 4) {
+            $stmt->bind_param($mode, $args[2], $args[3]);
+        }
+        else if ($args_count == 5) {
+            $stmt->bind_param($mode, $args[2], $args[3], $args[4]);
+        }
+        else if ($args_count == 6) {
+            $stmt->bind_param($mode, $args[2], $args[3], $args[4], $args[5]);
+        }
+        else if ($args_count == 7) {
+            $stmt->bind_param($mode, $args[2], $args[3], $args[4], $args[5], $args[6]);
+        }
+    }
+    $stmt->execute();
+    $stmt->store_result();
+    
+    bind_array($stmt, $row);
+    $array = [];
+    while ($stmt->fetch()) {
+        array_push($array, clone_array($row));
+    }
+    
+    $stmt->close();
+    $conn->close();
+    return $array;
+}
+
+// First argument is sql query, second is bind_param mode, after third is parameters.
+// Return: success -> true, error -> false
+function execute_query() {
+    $args = func_get_args();
+    $args_count = func_num_args();
+    if ($args_count == 1) {
+        $sql = $args[0];
+    }
+    else {
+        $sql = $args[0];
+        $mode = $args[1];
+    }
+    
+    global $db_host, $db_user, $db_pass, $db_name;
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    if ($conn->connect_errno) {
+        echo "<h1>데이터베이스에 연결하던 도중 오류가 발생했습니다.</h1>";
+        exit;
+    }
+    $stmt = $conn->prepare($sql);
+    if ($args_count != 1) {
+        if ($args_count == 3) {
+            $stmt->bind_param($mode, $args[2]);
+        }
+        else if ($args_count == 4) {
+            $stmt->bind_param($mode, $args[2], $args[3]);
+        }
+        else if ($args_count == 5) {
+            $stmt->bind_param($mode, $args[2], $args[3], $args[4]);
+        }
+        else if ($args_count == 6) {
+            $stmt->bind_param($mode, $args[2], $args[3], $args[4], $args[5]);
+        }
+        else if ($args_count == 7) {
+            $stmt->bind_param($mode, $args[2], $args[3], $args[4], $args[5], $args[6]);
+        }
+    }
+    $stmt->execute();
+    $stmt->store_result();
+
+    $stmt->close();
+    $conn->close();
+    if ($stmt->errno) {
+        return false; // error: false
+    }
+    else {
+        return true; // success: true
+    }
+}
+
+
