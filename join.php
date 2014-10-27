4f9640afcd653cb3f2ea7ee1e68214a650591de8
Kuriyama hibiya <kuriyama@hibiya.moe>
2014-10-27 13:07:49 +0000

    bunch of validation or something like that in php / ui rewrited

--- join.php
+++ join.php
@@ -8,8 +8,11 @@ $user_nickname = "";
 $user_email = "";
 
 if ($_POST) {
-    if (isset($_POST["user_id"]) && isset($_POST["user_pass"]) && isset($_POST["user_pass_twice"]) &&
-        isset($_POST["user_nickname"]) && isset($_POST["user_email"])
+    if (isset($_POST["user_id"]) && isset($_POST["user_pass"]) && 
+        isset($_POST["user_pass_twice"]) && isset($_POST["user_nickname"]) && 
+        isset($_POST["user_email"]) && !empty($_POST["user_id"]) && !empty($_POST["user_pass"]) && 
+        !empty($_POST["user_pass_twice"]) && !empty($_POST["user_nickname"]) && 
+        !empty($_POST["user_email"])
        ) {
         $user_id = $_POST["user_id"];
         $user_pass = $_POST["user_pass"];
@@ -19,13 +22,17 @@ if ($_POST) {
 
         $is_valid = true;
         if ($user_pass == $user_pass_twice) {
-            if (fetch_first_row("SELECT user_id FROM users WHERE user_id = ?", "s", $user_id) !== false) {
+            if (fetch_first_row("SELECT user_id FROM users WHERE user_id = ?", 
+                                "s", $user_id) !== false
+               ) {
                 $is_vaild = false;
                 $reason = "이미 존재하는 아이디입니다. 다른 아이디를 사용해 주세요.";
             }
 
             if ($is_valid == true) {
-                if (fetch_first_row("SELECT user_id FROM users WHERE user_email = ?", "s", $user_email) !== false) {
+                if (fetch_first_row("SELECT user_id FROM users WHERE user_email = ?", 
+                                    "s", $user_email) !== false
+                   ) {
                     $is_vaild = false;
                     $reason = "다른 사용자가 사용 중인 이메일입니다. 다른 이메일을 사용해 주세요.";
                 }
@@ -37,7 +44,10 @@ if ($_POST) {
         }
 
         if ($is_valid === true) {
-            if (execute_query("INSERT INTO users VALUES(NULL, ?, ?, ?, ?, DEFAULT, DEFAULT)", "ssss", $user_id, hash('sha512', $user_pass), $user_nickname, $user_email) === false) {
+            if (execute_query("INSERT INTO users VALUES(NULL, ?, ?, ?, ?, DEFAULT, DEFAULT)", 
+                              "ssss", $user_id, hash('sha512', $user_pass),
+                              $user_nickname, $user_email) === false
+               ) {
                 $is_vaild = false;
                 $reason = "DB 삽입 오류!";
             }
@@ -47,45 +57,49 @@ if ($_POST) {
             }
         }
     }
+    else {
+        $is_vaild = false;
+        $reason = "작성하지 않은 곳이 있습니다.";
+    }
 }
 
 //////////////////// HTML START ////////////////////
 
 require_once("header.php");
-if ($is_valid === false) {
-    echo "오류: " . $reason . "<br />";
-}
 ?>
-<form action="join.php" method="post">
-    <table cellspacing="0">
-        <tr>
-            <th>ID:</th>
-            <td><label><input type="text" name="user_id" value="<?= $user_id ?>"/></label></td>
-        </tr>
-        <tr>
-            <th>Password:</th>
-            <td><label><input type="password" name="user_pass"/></label></td>
-        </tr>
-        <tr>
-            <th>Password Again:</th>
-            <td><label><input type="password" name="user_pass_twice"/></label></td>
-        </tr>
-        <tr>
-            <th>Name:</th>
-            <td><label><input type="text" name="user_nickname"/ value="<?= $user_nickname ?>"></label></td>
-        </tr>
-        <tr>
-            <th>Email:</th>
-            <td><label><input type="email" name="user_email" value="<?= $user_email ?>"/></label></td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <input type="submit" value="전송">
-                <input type="button" onclick="history.back();" value="뒤로 가기" />
-                <!--a href="/login.php">로그인</a-->
-            </td>
-        </tr>
-    </table>
-</form>
+
+<div class="narrow-container">
+  <form class="form-list" action="login.php?join=done" method="post">
+    <h1>회원 가입</h1>
+    <?php if ($is_valid === false) { ?>
+      <p class="message message-error"><?= $reason ?></p>
+    <?php } ?>
+    <p class="form-line">
+      <label for="user_id">ID</label><!--
+      --><input type="text" name="user_id" required />
+    </p>
+    <p class="form-line">
+      <label for="user_pass">비밀번호</label><!--
+      --><input type="password" name="user_pass" required  />
+    </p>
+    <p class="form-line">
+      <label for="user_pass_twice">비밀번호 재입력</label><!--
+      --><input type="password" name="user_pass_twice" required  />
+    </p>
+    <p class="form-line">
+      <label for="user_nick">닉네임</label><!--
+      --><input type="text" name="user_nick" required  />
+    </p>
+    <p class="form-line">
+      <label for="user_email">e-mail</label><!--
+      --><input type="email" name="user_email" required  />
+    </p>
+    <p class="form-line">
+      <input type="submit" value="회원가입" class="button button-primary" />
+      <a href="/login.php" class="button">로그인</a>
+    </p>
+  </form>
+</div>
+
 </body>
 </html>
