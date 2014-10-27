b404c5a3d19670628cb0059934f0010884790d76
youngminz <youngminz.kr@gmail.com>
2014-10-27 03:21:59 +0000

    rewrite

--- board/exchange.php
+++ board/exchange.php
@@ -18,58 +18,52 @@ $total_page_count = $number_of_row % 10 != 0 ? $number_of_row / 10 + 1 : $number
 if ($page < 1 || $page > $total_page_count) {
     $page = 1;
 }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>:: Exchange Translation ::</title>
-    <style>
-        table, td, th {border: 1px solid black;border-collapse:collapse;}
-    </style>
-</head>
-<body>
-<?php
-$result = fetch_all_row("SELECT * FROM exchange_article WHERE parent_id IS NULL ORDER BY id DESC LIMIT ?, 10", "i", ($page - 1) * 10);
-?>
-<table>
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>글 제목</th>
-        <th>카테고리</th>
-        <th>번역 방향</th>
-        <th>날짜</th>
-        <th>조회수</th>
-        <th>Up Vote</th>
-        <th>Down Vote</th>
-        <th>작성자</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php 
-    foreach ($result as $row) { ?>
-        <tr>
-            <td><?= $row['ID'] ?></td>
-            <td>
-                <a href="/board/exchange_view.php?id=<?= $row["ID"] ?>"><?= $row['board_title'] ?></a>
-            </td>
-            <td><?= $row['category'] ?></td>
-            <td><?= $row['lang_from'] . ' -> ' . $row['lang_to'] ?></td>
-            <td><?= $row['date'] ?></td>
-            <td><?= $row['board_hit'] ?></td>
-            <td><?= $row['vote_up'] ?></td>
-            <td><?= $row['vote_down'] ?></td>
-            <td><?= fetch_first_row('SELECT * FROM users WHERE ID = ?', 'i', $row['author'])['user_nickname'] ?></td>
-        </tr>
-    <?php
-    } ?>
-    </tbody>
-</table>
-<?php
-for ($i = 1; $i <= $total_page_count; $i++) {
-    echo "<a href='/board/exchange.php?page=$i'>$i</a> ";
-}
-?>
+
+$result = fetch_all_row("SELECT * FROM exchange_article " . 
+                        "WHERE parent_id IS NULL ORDER BY id DESC LIMIT ?, 10",
+                        "i", ($page - 1) * 10);
+
+//////////////////// HTML START ////////////////////
+
+require_once("../header.php"); ?>
+<div class="container">
+    <div class="question">
+        <?php
+        foreach ($result as $row) { ?>
+            <div class="question-summary">
+                <div class="summary">
+                    <h3><a href="/board/exchange_view.php?id=<?= $row["ID"] ?>">
+                        <?= $row['board_title'] ?>
+                        </a></h3>
+                </div>
+                <div class="status">
+                    <span><?= $row['board_hit'] ?> 조회</span> / 
+                    <span>
+                        <?php echo fetch_first_row("SELECT COUNT(*) FROM exchange_article " . 
+                                                   "WHERE parent_id = ?",
+                                                   "i", $row['ID'])['COUNT(*)']; ?> 답변
+                    </span> / 
+                    <span><?= $row['vote_up'] + $row['vote_down'] ?> 평가</span>
+                </div>
+                <div class="info">
+                    <span><?= $row['category'] ?> 카테고리</span>, 
+                    <span><?= 
+            fetch_first_row('SELECT * FROM language WHERE lang_code = ?',
+                            's', $row['lang_from'])['korean'] . '에서 ' . 
+            fetch_first_row('SELECT * FROM language WHERE lang_code = ?',
+                            's', $row['lang_to'])['korean'] . '로' ?></span>,
+                    <span><?= $row['date'] . '에' ?></span>,
+                    <span><?= fetch_first_row('SELECT * FROM users WHERE ID = ?',
+                                    'i', $row['author'])['user_nickname'] . '가' ?></span>
+                </div>
+            </div>
+        <?php
+        }
+        for ($i = 1; $i <= $total_page_count; $i++) {
+            echo "<a href='/board/exchange.php?page=$i'>$i</a> ";
+        }
+    ?>
+    </div>
+</div>
 </body>
 </html>
