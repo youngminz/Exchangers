27d4adab33905e9435230bf45d2a9c7bd69edfc2
Kuriyama hibiya <kuriyama@hibiya.moe>
2014-10-28 12:10:52 +0000

    UI: almost done, TODO apply to responce
    UI: almost done, TODO apply to responce

--- board/exchange_view.php
+++ board/exchange_view.php
@@ -12,7 +12,15 @@ if (isset($_GET['id'])) {
     $article_id = (int) $_GET['id'];
 }
 
-#if (isset(_))
+/*
+if ($_POST) {
+    if (isset($_POST['contents']) && !empty($_POST['contents'])) {
+        $query = "INSERT INTO exchange_article " . 
+                 "VALUES ()"
+        execute_query()
+    }
+}
+*/
 
 $question = fetch_first_row("SELECT * FROM exchange_article WHERE id = ?", "i", $article_id);
 $answer = fetch_all_row("SELECT * FROM exchange_article WHERE parent_id = ?", "i", $article_id);
@@ -32,7 +40,7 @@ function recursive_comment($parent_article, $parent_id, $level) {
     
     if ($parent_id === NULL) { ?>
     <form method="post" action="/board/write_comment.php">
-      <input type="text" name="contents" />
+      <input type="text" name="text" placeholder="댓글을 입력하세요..." />
       <input type="hidden" name="mode" value="exchange" />
       <input type="hidden" name="parent_id" value="NULL" />
       <input type="hidden" name="parent_article" value="<?= $parent_article ?>" />
@@ -40,7 +48,7 @@ function recursive_comment($parent_article, $parent_id, $level) {
 <?php }
     if (count($result) > 0) {
         foreach ($result as $row) {
-            echo '<li style="margin-left: ' . $level*2 . 'rem;">';
+            echo '<li style="padding-left: ' . $level*1.5 . 'rem;">';
             if ($row['visible'] == 1) {
                 echo $row["content"];
                 echo " - <small>" .
@@ -52,23 +60,23 @@ function recursive_comment($parent_article, $parent_id, $level) {
                          class="icon icon-small">
                       <path d="M0,1h3v-1h5v1h3v1h-12z" />
                       <path d="M1,3h9v9h-1v-8h-1v7h-1v-7h-1v7h-1v-7h-1v7h-1v-7h-1v7h7v1h-8z" />
-                    </svg>
-                  </a>
+                    </svg></a>
+                <?php } ?>
                   <label for="toggle-visible-comment-<?= $row['ID'] ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"
                          class="icon icon-small">
-                      <path d="M0,6 v6 h6 v-1 h-5 m1,12 m0,11 m11,1 v-5 z" />
+                      <path d="M1,4v7h7m-7,0L11,1z" stroke-width="2" stroke="#000" fill="none" />
                     </svg>
                   </label>
-                  <input type="checkbox" style="display: hidden;"
+                  <input type="checkbox" style="display: none;"
                          id="toggle-visible-comment-<?= $row['ID'] ?>" />
                   <form method="post" action="/board/write_comment.php">
-                    <input type="text" name="text" />
+                    <input type="text" name="text" placeholder="댓글을 입력하세요..." />
                     <input type="hidden" name="mode" value="exchange" />
                     <input type="hidden" name="parent_id" value="<?= $row['ID'] ?>" />
                     <input type="hidden" name="parent_article" value="<?= $parent_article ?>" />
                   </form>
-  <?php }
+  <?php
     } else echo "<i>[삭제된 댓글입니다]</i>";
             recursive_comment($parent_article, $row['ID'], $level + 1);
         }
@@ -89,7 +97,7 @@ require_once('../header.php');
     </section>
   </aside>
   <article class="question">
-    <div class="vote">
+    <div class="vote left">
       <a href="/board/exchange_vote.php?mode=exchange&type=up&article=<?= $question['ID'] ?>">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 12" class="icon">
 	        <polygon points="12,0 24,12 23,13 12,1.41423 1,13 0,12 "/>
@@ -102,23 +110,22 @@ require_once('../header.php');
         </svg>
       </a>
     </div>
-    <div>
-      <section class="content">
-        <?= $question['contents'] ?>
-      </section>
-      <section class="comments">
-       <?php recursive_comment($question['ID'], NULL, 1); ?>
-      </section>
-    </div>
+    <section class="content">
+      <?= $question['contents'] ?>
+    </section>
+    <span class="left">댓글</span>
+    <section class="comment">
+      <?php recursive_comment($question['ID'], NULL, 1); ?>
+    </section>
   </article>
   
-  답변:
 <?php
 if (count($answer) == 0) {
-  echo "아직 달린 답변이 없습니다. ";
-} else foreach ($answer as $answer_row) {?>
+  echo "답변이 없습니다. ";
+} else {
+  echo "<h2>답변 " . count($answer) . "개</h2>";
+  foreach ($answer as $answer_row) { ?>
   <article>
-    <hr>
     <div class="vote">
       <a href="/board/exchange_vote.php?type=up&article=<?= $answer_row['ID'] ?>">Up vote</a>
       <a href="/board/exchange_vote.php?type=down&article=<?= $answer_row['ID'] ?>">Down vote</a>
@@ -127,19 +134,22 @@ if (count($answer) == 0) {
       <section class="content">
         <?= $answer_row['contents'] ?>
       </section>
-      <section class="comments">
+      <section class="comment">
        <?php recursive_comment($answer_row['ID'], NULL, 1); ?>
       </section>
     </div>
+    <form class="form-write" action="/board/exchange_view.php" method="post">
+      <textarea name="contents"></textarea>
+      <p class="form-line">
+        <a href="/board/exchange.php" class="button">목록</a>
+        <input type="submit" value="작성" class="button-primary" />
+      </p>
+    </form>
   </article>
-<?php } ?>
-  <form class="form-write" action="/board/exchange_view.php" method="post">
-    <textarea name="contents"></textarea>
-    <p class="form-line">
-      <a href="/board/exchange.php" class="button">목록</a>
-      <input type="submit" value="작성" class="button-primary" />
-    </p>
-  </form>
+<?php
+  }
+}
+?>
 </main>
 
 </body>
