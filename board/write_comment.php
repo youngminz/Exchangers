c47bae1d3ebb28f029a731bdc7eb00636cc54293
Kuriyama hibiya <kuriyama@hibiya.moe>
2014-10-28 10:23:10 +0000

    contents (variable/POST argname) -> text

--- board/write_comment.php
+++ board/write_comment.php
@@ -8,16 +8,16 @@ if (!isset($_SESSION['ID']) || empty($_SESSION['ID'])) {
 }
 if ($_POST) {
     if (isset($_POST['mode']) && isset($_POST['parent_id']) && 
-        isset($_POST['contents']) && isset($_POST['parent_article']) &&
+        isset($_POST['text']) && isset($_POST['parent_article']) &&
         !empty($_POST['mode']) && !empty($_POST['parent_id']) && 
-        !empty($_POST['contents']) && !empty($_POST['parent_article']) && 
+        !empty($_POST['text']) && !empty($_POST['parent_article']) && 
         $_POST['mode'] === 'exchange'
        ) {
         if ($_POST['parent_id'] == 'NULL') {
             $_POST['parent_id'] = NULL;
         }
         $query = "INSERT INTO exchange_comment VALUES (NULL, ?, DEFAULT, ?, 1, ?, ?)";
-        $result = execute_query($query, "siii", htmlspecialchars($_POST['contents']), 
+        $result = execute_query($query, "siii", htmlspecialchars($_POST['text']), 
                                 $_SESSION['ID'], $_POST['parent_id'], $_POST['parent_article']);
         if ($result === false) {
             header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
