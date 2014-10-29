b934a3c64dc39b4d0f41e94fb209a544f333a0fe
Kuriyama hibiya <kuriyama@hibiya.moe>
2014-10-29 14:42:46 +0000

    ui enhancement: update profile

--- profile.php
+++ profile.php
@@ -19,10 +19,6 @@ $modified = false;
 $different_password = false;
 
 if ($_POST) {
-    if (isset($_POST['user_id']) && !empty($_POST['user_id'])) {
-        execute_query('UPDATE users SET user_id = ? WHERE ID = ?', 'si', htmlspecialchars($_POST['user_id']), $_SESSION['ID']);
-        $modified = true;
-    }
     if (isset($_POST['user_pass']) && !empty($_POST['user_pass']) &&
         isset($_POST['user_pass_twice']) && !empty($_POST['user_pass_twice'])) {
         if ($_POST['user_pass'] == $_POST['user_pass_twice']) {
@@ -37,10 +33,10 @@ if ($_POST) {
         execute_query('UPDATE users SET user_nickname = ? WHERE ID = ?', 'si', htmlspecialchars($_POST['user_nickname']), $_SESSION['ID']);
         $modified = true;
     }
-    if (isset($_POST['user_email']) && !empty($_POST['user_email'])) {
+    /*if (isset($_POST['user_email']) && !empty($_POST['user_email'])) {
         execute_query('UPDATE users SET user_email = ? WHERE ID = ?', 'si', htmlspecialchars($_POST['user_email']), $_SESSION['ID']);
         $modified = true;
-    }
+    }*/
     $my_data = fetch_first_row("SELECT * FROM users WHERE ID = ?", 'i', $_SESSION['ID']);
 
     $_SESSION['user_id'] = $my_data['user_id'];
@@ -61,6 +57,29 @@ require_once('header.php');
     <div>
       <h1><?= $profile_data['user_nickname'] ?></h1>
       사용자 #<?= $id ?>
+      <form class="form-list" action="profile.php" method="post">
+        <?php if ($modified === true) { ?>
+          <p class="message message-success">정보 변경이 성공적으로 이루어졌습니다.</p>
+        <?php } if ($different_password === true) { ?>
+          <p class="message message-error">변경하려는 패스워드가 일치하지 않습니다!</p>
+        <?php } ?>
+        <p class="form-line">
+          <label for="user_pass">비밀번호</label><!--
+       --><input type="password" name="user_pass"  />
+        </p>
+        <p class="form-line">
+          <label for="user_pass_twice">재입력</label><!--
+       --><input type="password" name="user_pass_twice"  />
+        </p>
+        <p class="form-line">
+          <label for="user_nickname">닉네임</label><!--
+       --><input type="text" name="user_nickname" placeholder="<?= $profile_data['user_nickname'] ?>"  />
+        </p>
+        <p class="form-line">
+          <a href="/leave_ask.php" class="button">회원 탈퇴</a>
+          <input type="submit" value="프로필 업데이트" class="button button-primary right" />
+        </p>
+      </form>
       <dl>
         <dt>아이디</dt>
         <dd><?= $profile_data['user_id'] ?></dd>
@@ -76,38 +95,7 @@ require_once('header.php');
     </div>
   </header>
   <?php if ($is_me === true) { ?>
-      <form class="form-list" action="profile.php" method="post">
-        <h1>내 프로필</h1>
-        <?php if ($modified === true) { ?>
-          <p class="message message-success">정보 변경이 성공적으로 이루어졌습니다.</p>
-        <?php } if ($different_password === true) { ?>
-          <p class="message message-error">변경하려는 패스워드가 일치하지 않습니다!</p>
-        <?php } ?>
-        <p class="form-line">
-          <label for="user_id">ID</label><!--
-       --><input type="text" name="user_id" placeholder="<?= $profile_data['user_id'] ?>" />
-        </p>
-        <p class="form-line">
-          <label for="user_pass">비밀번호</label><!--
-       --><input type="password" name="user_pass"  />
-        </p>
-        <p class="form-line">
-          <label for="user_pass_twice">비밀번호 재입력</label><!--
-       --><input type="password" name="user_pass_twice"  />
-        </p>
-        <p class="form-line">
-          <label for="user_nickname">닉네임</label><!--
-       --><input type="text" name="user_nickname" placeholder="<?= $profile_data['user_nickname'] ?>"  />
-        </p>
-        <p class="form-line">
-          <label for="user_email">e-mail</label><!--
-       --><input type="email" name="user_email" placeholder="<?= $profile_data['user_email'] ?>" />
-        </p>
-        <p class="form-line">
-          <a href="/leave_ask.php" class="button">회원 탈퇴</a>
-          <input type="submit" value="프로필 업데이트" class="button button-primary right" />
-        </p>
-      </form>
+      
 <?php } ?>  
 </main>
 </body>
